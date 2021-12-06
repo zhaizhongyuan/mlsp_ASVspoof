926259c37d0ab662a574af9600d8fab10a17d658
Yuxuan Wu <wu_yuxuan201@163.com>
2021-12-05 22:53:50 -0500

    update silence

--- data_processing.py
+++ data_processing.py
@@ -11,38 +11,74 @@ import spafe.features.bfcc
 import spafe.features.gfcc
 import spafe.features.ngcc
 from tqdm import tqdm
+import silence_measure
 from ctypes import c_int
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--data_path", required=True, type=str, help='path to ASVSpoof data directory. For example, LA/ASVspoof2019_LA_train/flac/', default="/mnt/LA/ASVspoof2019_LA_train/flac/")
-parser.add_argument("--label_path", required=True, type=str, help='path to label file. For example, LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.train.trn.txt', default="/mnt/LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.train.trn.txt")
-parser.add_argument("--output_path", required=True, type=str, help='path to output pickle file. For example, ./data/train.pkl', default="/home/yuxuan/MLSP_ASVspoof/data/train")
-parser.add_argument("--ftype", required=True, type=str, help="type of feature. For example, lfcc, mfcc, silence, ...")
+parser.add_argument(
+    "--data_path",
+    required=True,
+    type=str,
+    help="path to ASVSpoof data directory. For example, LA/ASVspoof2019_LA_train/flac/",
+    default="/mnt/LA/ASVspoof2019_LA_train/flac/",
+)
+parser.add_argument(
+    "--label_path",
+    required=True,
+    type=str,
+    help="path to label file. For example, LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.train.trn.txt",
+    default="/mnt/LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.train.trn.txt",
+)
+parser.add_argument(
+    "--output_path",
+    required=True,
+    type=str,
+    help="path to output pickle file. For example, ./data/train.pkl",
+    default="/home/yuxuan/MLSP_ASVspoof/data/train",
+)
+parser.add_argument(
+    "--ftype",
+    required=True,
+    type=str,
+    help="type of feature. For example, lfcc, mfcc, silence, ...",
+)
 args = parser.parse_args()
+
+
+def increment():
+    # Multiprocess counter
+    with counter_lock:
+        counter.value += 1
+        if counter.value % 200 == 0:
+            print(counter.value)
 
 
 # modify data processing to calculate coefficient: MFCC, delta MFCC, and delta delta MFCC
 def calculate_delta(array):
     # https://github.com/MohamadMerchant/Voice-Authentication-and-Face-Recognition
-    rows,cols = array.shape
-    deltas = np.zeros((rows,20))
+    rows, cols = array.shape
+    deltas = np.zeros((rows, 20))
     N = 2
     for i in range(rows):
         index = []
         j = 1
         while j <= N:
-            if i-j < 0:
+            if i - j < 0:
                 first = 0
             else:
-                first = i-j
-            if i+j > rows -1:
-                second = rows -1
+                first = i - j
+            if i + j > rows - 1:
+                second = rows - 1
             else:
-                second = i+j
-            index.append((second,first))
-            j+=1
-        deltas[i] = (array[index[0][0]]-array[index[0][1]] + (2 * (array[index[1][0]]-array[index[1][1]]))) / 10
+                second = i + j
+            index.append((second, first))
+            j += 1
+        deltas[i] = (
+            array[index[0][0]]
+            - array[index[0][1]]
+            + (2 * (array[index[1][0]] - array[index[1][1]]))
+        ) / 10
     return deltas
 
 
@@ -55,24 +91,76 @@ def process_audio(filepath):
         filename2label[filename] = label
 
     # Read audio file
-    filename = filepath.split('.')[0]
+    filename = filepath.split(".")[0]
     if filename not in filename2label:
-         # we skip speaker enrollment stage
+        # we skip speaker enrollment stage
         return
     label = filename2label[filename]
     sig, rate = sf.read(os.path.join(args.data_path, filepath))
 
+    increment()
+
     # extract feature, delta of feature, delta delta of feature
+    if args.ftype == "silence":
+        feat = silence_measure.get_silence(sig, rate)
+        feat = feat.reshape((1, 2))
+        return feat, label
+
     if args.ftype == "lfcc":
-        feat = spafe.features.lfcc.lfcc(sig, fs=rate, num_ceps=20, pre_emph=0, win_len=0.03, win_hop=0.015, nfilts=70, nfft=1024)
+        feat = spafe.features.lfcc.lfcc(
+            sig,
+            fs=rate,
+            num_ceps=20,
+            pre_emph=0,
+            win_len=0.03,
+            win_hop=0.015,
+            nfilts=70,
+            nfft=1024,
+        )
     elif args.ftype == "mfcc":
-        feat = spafe.features.mfcc.mfcc(sig, fs=rate, num_ceps=20, pre_emph=0, win_len=0.03, win_hop=0.015, nfilts=70, nfft=1024)
+        feat = spafe.features.mfcc.mfcc(
+            sig,
+            fs=rate,
+            num_ceps=20,
+            pre_emph=0,
+            win_len=0.03,
+            win_hop=0.015,
+            nfilts=70,
+            nfft=1024,
+        )
     elif args.ftype == "bfcc":
-        feat = spafe.features.bfcc.bfcc(sig, fs=rate, num_ceps=20, pre_emph=0, win_len=0.03, win_hop=0.015, nfilts=70, nfft=1024)
+        feat = spafe.features.bfcc.bfcc(
+            sig,
+            fs=rate,
+            num_ceps=20,
+            pre_emph=0,
+            win_len=0.03,
+            win_hop=0.015,
+            nfilts=70,
+            nfft=1024,
+        )
     elif args.ftype == "gfcc":
-        feat = spafe.features.gfcc.gfcc(sig, fs=rate, num_ceps=20, pre_emph=0, win_len=0.03, win_hop=0.015, nfilts=70, nfft=1024)
+        feat = spafe.features.gfcc.gfcc(
+            sig,
+            fs=rate,
+            num_ceps=20,
+            pre_emph=0,
+            win_len=0.03,
+            win_hop=0.015,
+            nfilts=70,
+            nfft=1024,
+        )
     elif args.ftype == "ngcc":
-        feat = spafe.features.ngcc.ngcc(sig, fs=rate, num_ceps=20, pre_emph=0, win_len=0.03, win_hop=0.015, nfilts=70, nfft=1024)
+        feat = spafe.features.ngcc.ngcc(
+            sig,
+            fs=rate,
+            num_ceps=20,
+            pre_emph=0,
+            win_len=0.03,
+            win_hop=0.015,
+            nfilts=70,
+            nfft=1024,
+        )
     else:
         print("Bad feature type!")
     delta_feat = calculate_delta(feat)
@@ -82,7 +170,9 @@ def process_audio(filepath):
     return combined_feat, label
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    counter = multiprocessing.Value(c_int)
+    counter_lock = multiprocessing.Lock()
     # Multiprocess to prepare data
     a_pool = multiprocessing.Pool(8)
     feat_label = a_pool.map(process_audio, os.listdir(args.data_path))
@@ -92,7 +182,7 @@ if __name__ == '__main__':
         os.makedirs(args.output_path)
 
     # Save output data
-    output_path = os.path.join(args.output_path, args.ftype+".pkl")
-    with open(output_path, 'wb') as outfile:
+    output_path = os.path.join(args.output_path, args.ftype + ".pkl")
+    with open(output_path, "wb") as outfile:
         pickle.dump(feat_label, outfile)
         print("dumpped", output_path)
