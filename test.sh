784661891e6c639985698e04350e9557eee17da2
yxzi <zzz888rrr@gmail.com>
2021-12-07 22:27:43 -0500

    update shells, add tdcf tool folder

--- test.sh
+++ test.sh
@@ -1,9 +1,18 @@
-echo "testing with dev data on features: mfcc"
-python3 gmm_test.py --data_path ./data/dev/mfcc.pkl \
-                    --model_path_bon ./model/mfcc/bon.gmm \
-                    --model_path_sp ./model/mfcc/sp.gmm
+# echo "testing with dev data on features: mfcc"
+# python3 gmm_test.py --data_path ./data/dev/mfcc.pkl \
+#                     --model_path_bon ./model/mfcc/bon.gmm \
+#                     --model_path_sp ./model/mfcc/sp.gmm
 
-echo "testing with test data on features: mfcc"
-python3 gmm_test.py --data_path ./data/test/mfcc.pkl \
-                    --model_path_bon ./model/mfcc/bon.gmm \
-                    --model_path_sp ./model/mfcc/sp.gmm
+# echo "testing with test data on features: mfcc"
+# python3 gmm_test.py --data_path ./data/test/mfcc.pkl \
+#                     --model_path_bon ./model/mfcc/bon.gmm \
+#                     --model_path_sp ./model/mfcc/sp.gmm
+
+python gmm_test_output.py --data_path ./data/eval/eval-mfcc.pkl \
+                         --model_path_bon ./model/mfcc/bon.gmm \
+                         --model_path_sp ./model/mfcc/sp.gmm \
+                         --label_path ../LA/ASVspoof2019_LA_cm_protocols/ASVspoof2019.LA.cm.eval.trl.txt \
+                         --output_path ./output/mfcc-eval.text
+
+python score_eval.py --cm_score ./output/mfcc-eval.text \
+                    --asv_score ./tDCF_python/scores/ASVspoof2019_LA_eval_asv_scores.txt
