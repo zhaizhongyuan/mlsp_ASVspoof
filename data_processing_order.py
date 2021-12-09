c05d44f83c3c58ce38b84eacf61e64993d155f95
yxzi <zzz888rrr@gmail.com>
2021-12-09 04:11:17 -0500

    ensemble logic

--- data_processing_order.py
+++ data_processing_order.py
@@ -161,6 +161,17 @@ def process_audio(filepath_tuple):
             nfilts=70,
             nfft=1024,
         )
+    elif args.ftype == "mfcc-25":
+        feat = spafe.features.mfcc.mfcc(
+            sig,
+            fs=rate,
+            num_ceps=20,
+            pre_emph=0,
+            win_len=0.025,
+            win_hop=0.01,
+            nfilts=70,
+            nfft=1024,
+        )
     elif args.ftype == "bfcc":
         feat = spafe.features.bfcc.bfcc(
             sig,
